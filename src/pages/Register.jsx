ce77aec32701053a43cd67c7f8366399d5b86ee6
shankha7139 <shankha7139@gmail.com>
2025-02-26 10:14:38 +0530

    register update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,15 @@
+// RegisterPage.js
 import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [dob, setDob] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
@@ -12,8 +17,23 @@ const RegisterPage = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      navigate("/");
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      await addDoc(collection(db, "customers"), {
+        uid: user.uid,
+        name,
+        email,
+        phoneNumber,
+        dob,
+      });
+
+      navigate("/home");
     } catch (err) {
       setError("Failed to register");
     }
@@ -26,12 +46,42 @@ const RegisterPage = () => {
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <form onSubmit={handleRegister}>
           <div className="mb-4">
+            <label className="block text-gray-700">Name</label>
+            <input
+              type="text"
+              className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
             <label className="block text-gray-700">Email</label>
             <input
               type="email"
               className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Phone Number</label>
+            <input
+              type="tel"
+              className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Date of Birth</label>
+            <input
+              type="date"
+              className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
               required
             />
           </div>
@@ -52,14 +102,6 @@ const RegisterPage = () => {
             >
               Register
             </button>
-          </div>
-          <div
-            className="text-blue-400 cursor-pointer hover:underline "
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            Login?
           </div>
         </form>
       </div>
